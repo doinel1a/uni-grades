2699750e25c341a46bcd9a243b581860a8a921bd
doinel1a <doinel1atanasiu@gmail.com>
2022-07-05 19:30:28 +0200

    Mantine Component library added

--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -1,19 +1,29 @@
 import type { AppProps } from 'next/app';
 
+import { MantineProvider } from '@mantine/core';
+import { NotificationsProvider } from '@mantine/notifications';
+
 import { ContextProvider } from '../contexts/ContextProvider';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 import '../styles/globals.css';
+import { ModalsProvider } from '@mantine/modals';
 
 const App = ({ Component, pageProps }: AppProps) => {
     return(
         <div className='w-screen h-screen'>
-            <ContextProvider>
-                <Header />
-                <Component {...pageProps} />
-                <Footer />
-            </ContextProvider>
+            <MantineProvider>
+                <ModalsProvider>
+                    <NotificationsProvider>
+                        <ContextProvider>
+                            <Header />
+                            <Component {...pageProps} />
+                            <Footer />
+                        </ContextProvider>
+                    </NotificationsProvider>
+                </ModalsProvider>
+            </MantineProvider>
         </div>
     );
 };
