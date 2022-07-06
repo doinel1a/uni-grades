449a881b25a6f8c80d03fe8d9114bb0c33ef3a0c
doinel1a <doinel1atanasiu@gmail.com>
2022-07-06 15:04:27 +0200

    Row component created & implemented

--- front-end/components/Main.tsx
+++ front-end/components/Main.tsx
@@ -1,12 +1,12 @@
-import { v4 as uuid } from 'uuid';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 import { Exam } from '../interfaces/Exam';
 import { useStateContext } from '../contexts/ContextProvider';
 import Paragraph from './HtmlElements/Paragraph';
 
 import Numbers from '../utils/Numbers';
+import Row from './Row';
+import Modal from './Modal';
 
 interface IProps {
     data: Exam[];
@@ -14,6 +14,8 @@ interface IProps {
 
 const Main: React.FC<IProps> = ({ data }) => {
     const { isDarkMode } = useStateContext();
+
+    const [isEditRow, setIsEditRow] = useState<boolean>(false);
 
     let totPoints: number = 0;
     let totCredits: number = 0;
@@ -25,18 +27,24 @@ const Main: React.FC<IProps> = ({ data }) => {
         totGrades += _data.grade;
     });
 
+    const deleteExamHandler: Function = (_e: MouseEvent, _id: string) => {
+        _e.preventDefault();
+        <Modal />
+        console.log(_id);
+    };
+
     return(
         <main className='container h-full flex flex-col lg:flex-row px-1'>
             <section className={` container lg:w-[700px] h-1/4 lg:h-56 p-2 lg:p-4 mb-10 lg:mt-24 lg:mr-10 border-x-2 lg:border-2 rounded-b-xl lg:rounded-xl border-white transition-colors ${ isDarkMode ? 'bg-zinc-800' : 'bg-gray-300' } `}>
                 <div className='flex justify-center items-center mb-4 rounded-lg font-semibold'>
                     <Paragraph 
                         text='Welcome'
-                        customCss='mr-1 text-2xl lg:text-4xl'
+                        customCss='mr-1 !text-2xl lg:!text-4xl'
                     />
                     <br />
                     <Paragraph 
                         text='Doinel Atanasiu'
-                        customCss='ml-1 text-2xl lg:text-4xl'
+                        customCss='ml-1 !text-2xl lg:!text-4xl'
                     />
                 </div>
                 <div className='flex rounded-lg'>
@@ -85,46 +93,20 @@ const Main: React.FC<IProps> = ({ data }) => {
                 <div className='text-left mb-2 lg:mb-4'>
                     <Paragraph 
                         text='EXAMS:'
-                        customCss='text-2xl lg:text-4xl'
+                        customCss='!text-2xl lg:!text-4xl'
                     />
                 </div>
-                <div className='w-full h-full flex flex-col gap-y-2 p-1 border-x-2 border-t-2 rounded-t-xl overflow-auto'>
+                <div className='w-full main-h flex flex-col gap-y-2 lg:gap-y-4 p-1 pr-2 lg:p-3 lg:pr-5 border-x-2 border-t-2 rounded-t-xl overflow-auto lg:overflow-hidden lg:hover:overflow-auto'>
                 {
                     data.map( _data => (
-                        <div key={ uuid() } className={` w-full h-36 flex justify-between rounded-lg transition-colors duration-1000 ${ isDarkMode ? 'bg-zinc-800' : 'bg-gray-300' } `}>
-                            <div className='w-3/4 flex flex-col gap-y-4 p-3'>
-                                <Paragraph 
-                                    text={ _data.subject }
-                                    customCss='text-lg lg:text-xl text-left font-bold'
-                                />
-                                <Paragraph 
-                                    text={`${ _data.credits } Credits`}
-                                    customCss='text-lg lg:text-xl'
-                                />
-                            </div>
-                            <div className='w-1/4 lg:w-1/6 flex flex-col'>
-                                <div className='w-full h-1/3 flex justify-center items-center rounded-tr-lg rounded-bl-lg bg-red-500'>
-                                    <Paragraph 
-                                        text={`${ _data.grade }`}
-                                        customCss='text-2xl lg:text-3xl'
-                                    />
-                                </div>
-                                <div className='w-full h-2/3 flex flex-col lg:flex-row justify-around items-end lg:pb-4 px-2 py-1'>
-                                    <button type='button'>
-                                        <FontAwesomeIcon
-                                            icon={ faPenToSquare }
-                                            className={` w-7 lg:w-8 h-7 lg:h-8 ${ isDarkMode ? 'text-white' : 'text-[#333]'} `}
-                                        />
-                                    </button>   
-                                    <button type='button'>
-                                        <FontAwesomeIcon
-                                            icon={ faTrash }
-                                            className={` w-7 lg:w-8 h-7 lg:h-8 ${ isDarkMode ? 'text-white' : 'text-[#333]'} `}
-                                        />
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
+                        <Row
+                            key={ _data._id}
+                            id={ _data._id}
+                            subject={ _data.subject }
+                            credits={ _data.credits }
+                            grade={ _data.grade }
+                            deleteExamHandler={ deleteExamHandler }
+                        />
                     ))
                 }
                 </div>
---
+++ front-end/components/Row.tsx
@@ -0,0 +1,61 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
+
+import { useStateContext } from '../contexts/ContextProvider';
+import Paragraph from './HtmlElements/Paragraph';
+import Link from 'next/link';
+
+interface IRowProps {
+    id: string;
+    subject: string;
+    credits: number;
+    grade: number;
+    deleteExamHandler: Function;
+};
+
+const Row: React.FC<IRowProps> = ({ id, subject, credits, grade, deleteExamHandler }) => {
+    const { isDarkMode } = useStateContext();
+
+    return(
+        <div className={` w-full h-36 flex justify-between flex-shrink-0 rounded-lg transition-colors ${ isDarkMode ? 'bg-zinc-800' : 'bg-gray-300' } `}>
+            <div className='w-3/4 flex flex-col gap-y-4 p-3'>
+                <Paragraph 
+                    text={ subject }
+                    customCss='text-lg lg:text-xl text-left font-bold'
+                />
+                <Paragraph 
+                    text={`${ credits } Credits`}
+                    customCss='text-lg lg:text-xl'
+                />
+            </div>
+            <div className='w-1/4 lg:w-1/6 flex flex-col'>
+                <div className='w-full h-1/3 flex justify-center items-center rounded-tr-lg rounded-bl-lg bg-red-500'>
+                    <Paragraph 
+                        text={`${ grade }`}
+                        customCss='text-2xl lg:text-3xl'
+                    />
+                </div>
+                <div className='w-full h-2/3 flex flex-col lg:flex-row justify-around items-end lg:pb-4 px-2 py-1'>
+                    <button type='button' aria-label="Edit exam's details">
+                        <Link href={ `/${encodeURIComponent(id)}` }>
+                            <a>
+                                <FontAwesomeIcon
+                                    icon={ faPenToSquare }
+                                    className={` w-7 lg:w-8 h-7 lg:h-8 transition-colors  ${ isDarkMode ? 'text-white' : 'text-[#333]'} `}
+                                />
+                            </a>
+                        </Link>
+                    </button>   
+                    <button type='button' aria-label="Delete exam" onClick={ _e => deleteExamHandler(_e, id)}>
+                        <FontAwesomeIcon
+                            icon={ faTrash }
+                            className={` w-7 lg:w-8 h-7 lg:h-8 transition-colors  ${ isDarkMode ? 'text-white' : 'text-[#333]'} `}
+                        />
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Row;
